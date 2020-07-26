2e706ab58cf5910a4002e9888de1b7364672f43a
vitorregisr <vitorregisrr@gmail.com>
2020-07-26 18:13:36 -0300

    Add home and responsive menu

--- js/misc.js
+++ js/misc.js
@@ -5,15 +5,16 @@
     new WOW().init();
     $('.lazy').lazyload();
 
-    // scroll top var scrollTopBtn = $('.scroll-top'); $(window).scroll(function ()
-    // {     if ($(window).scrollTop() > 300) { scrollTopBtn.addClass('show');     }
-    // else { scrollTopBtn.removeClass('show');     } }); scrollTopBtn.on('click',
-    // function (e) {     e.preventDefault();     $('html, body').animate({
-    // scrollTop: 0     }, '300'); }); Smooth Scrooling
-    // $('a[href*=\\#]:not([href$=\\#])').click(function () {
-    // event.preventDefault();     $('html, body').animate({         scrollTop:
-    // $($.attr(this, 'href'))             .offset()             .top - 80     },
-    // 500); }); Trumps JS
+    // Sticky header
+    $(document).on("scroll", function () {
+        if ($(document).scrollTop() > 150) {
+            $("#main-header").addClass("sticky");
+        } else if (!($(".navbar-collapse").hasClass('show'))) {
+            $("#main-header").removeClass("sticky");
+
+        }
+    });
+
     $('[data-toggle="expand-markup"]').click(function () {
         $('.markup').addClass('expanded');
         $(this).fadeOut();
--- js/responsivenav.js
+++ js/responsivenav.js
@@ -1,10 +1,10 @@
 (function () {
     'use strict';
 
-    $('#nav-toggler').click(function () {
+    $('[data-toggle="nav"]').click(function () {
         const isExpanded = $(this).attr('aria-expanded') === 'false' ? false : true;
 
-        $('#mobile-nav').toggleClass('active');
+        $('#nav-content').toggleClass('active');
 
         $(this).attr('aria-expanded', !isExpanded);
     });
