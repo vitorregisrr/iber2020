c65d9c7f7d66f2f66a9a18cbb9288e3b6e3d26a4
vitorregisr <vitorregisrr@gmail.com>
2020-07-27 01:39:46 -0300

    Add complete home

--- js/carousels.js
+++ js/carousels.js
@@ -7,7 +7,7 @@
         margin: 0,
         autoHeight: true,
         center: false,
-        margin: 30,
+        margin: 20,
         nav: true,
         dots: false,
         startPosition: 0,
@@ -25,11 +25,39 @@
             },
             
             768: {
-                items: 3,
+                items: 4,
+                margin: 10
+            },
+            1098: {
+                margin: 0,
+                items: 6,
+            }
+        }
+    });
+    // Home Carousel //
+    const bannerCaixa = $(".banner-caixa__carousel");
+    bannerCaixa.owlCarousel({
+        margin: 0,
+        autoHeight: true,
+        center: false,
+        margin: 30,
+        nav: true,
+        dots: false,
+        startPosition: 0,
+        infinite: true,
+        loop: true,
+        navText: ['<i class="fa fa-arrow-left"></i>', '<i class="fa fa-arrow-right"></i>'],
+        responsive: {
+            300: {
+                items: 1,
+                margin: 15
+            },
+            768: {
+                items: 2,
                 margin: 30
             },
             1098: {
-                items: 6,
+                items: 3,
             }
         }
     });
