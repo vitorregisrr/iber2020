cbf22ced08a290afc7badb1554b545a45e29c289
vitorregisr <vitorregisrr@gmail.com>
2020-08-04 00:29:48 -0300

    Misc improves

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// Use apenas em um enviroment com webpack!
-// require('./misc');
-// require('./carousels');
-// require('./inputmasks');
-// require('./forms');
-// require('./dropdowns');
-// require('./modal');
-// require('./login');
-// require('./responsivenav');
